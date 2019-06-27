e43ce0c46a6b36b0174eb679eaf0afc38d2bc409
Philippe Loctaux <p@philippeloctaux.com>
2019-06-27 11:42:19 +0200

    added function to manually add user to database

---
+++ util/manualAddUser.js
@@ -0,0 +1,29 @@
+var bcrypt = require('bcryptjs');
+
+// user model
+var User = require('../models/user');
+
+module.exports = function () {
+  var newUser = new User({
+    id: require('uuid/v4')(),
+    firstName: "James",
+    lastName: "Smith",
+    email: "james.smith@example.net",
+    phone: "+1 206 555 010",
+    address: "1 Infinite Loop, Cupertino, CA 95014 USA",
+    password: "hellopassword",
+    referent: "JS001"
+  });
+
+  // generate salt and hash password
+  bcrypt.genSalt(10, (err, salt) =>
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      // store hashed password
+      newUser.password = hash;
+      // save new user
+      newUser.save()
+        .then(user => { console.log(`user ${user} created`) })
+        .catch(err => console.error(err));
+    }));
+}
