6b93982f4d85d40e549a50eb46904a99eddbd9c5
Philippe Loctaux <p@philippeloctaux.com>
2019-07-03 16:07:53 +0200

    double quotes instead of simple

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -1,4 +1,4 @@
-export const status: string = (process.env.NODE_ENV === 'production') ? 'production' : 'development';
+export const status: string = (process.env.NODE_ENV === "production") ? "production" : "development";
 export const port: number = 8002;
 export const siteName: string = "puffy-pocket";
 export const passwordLength: number = 12;
