1f684c4cf6b773f5312069eb78bfab77fdc66d38
Philippe Loctaux <p@philippeloctaux.com>
2019-07-03 15:37:26 +0200

    redone user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,44 +1,16 @@
-var mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-var UserSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true
-  },
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  phone: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  referent: {
-    type: String,
-    required: true
-  },
-  creationDate: {
-    type: Date,
-    default: Date.now
-  }
+const UserSchema = new mongoose.Schema({
+  // TODO: see if 'required' is really useful, see best practices for mongodb schema
+  id: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  password: { type: String, required: true },
+  referent: { type: String, required: true },
+  creationDate: { type: Date, default: Date.now }
 });
 
-var User = mongoose.model('User', UserSchema);
-
-module.exports = User;
+export const User = mongoose.model("User", UserSchema);
