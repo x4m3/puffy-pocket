8ebf12c64ce18fa7881355be8c2fdfdd547472c0
Philippe Loctaux <p@philippeloctaux.com>
2019-07-03 16:56:47 +0200

    rewrote referent code generation

--- src/util/generateReferentCode.ts
+++ src/util/generateReferentCode.ts
@@ -1,15 +1,22 @@
-module.exports = function (firstName: string, lastName: string, nb: number) {
-  if (!firstName || !lastName || nb == 0) {
-    throw new Error("bad parameters, make sure values are not empty");
+/**
+ * Generates a referent code based of a person's initials with a number of digits
+ * @param {string} firstName First name of person
+ * @param {string} lastName Last name of person
+ * @param {number} nb Number of digits in the referent code (optional, defaults to 3)
+ * @returns {string} Referent code
+ */
+export function generateReferentCode(firstName: string, lastName: string, nb?: number): string {
+  if (!nb || nb == 0) {
+    nb = 3;
   }
 
   // store first characters of firstName and lastName
-  var string = firstName.charAt(0) + lastName.charAt(0);
+  var referentCode: string = firstName.charAt(0) + lastName.charAt(0);
 
   for (var i = 0; i < nb; i++) {
     // generate a random digit and store it in the string
-    string += Math.floor(Math.random() * 9);
+    referentCode += Math.floor(Math.random() * 9);
   }
 
-  return string;
+  return referentCode;
 };
