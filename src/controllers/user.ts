6394d5ce22668aaf4aa200707be16d6de963c3ba
Philippe Loctaux <p@philippeloctaux.com>
2019-07-06 18:54:48 +0200

    added generateUserId, storing users correctly now

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,8 @@
 import { passwordLength } from "../config/config";
-import { User } from "../models/user";
 import { Request, Response, NextFunction } from "express";
+import { User } from "../models/user";
 import { generateReferentCode } from "../util/generateReferentCode";
-import bcrypt from "bcryptjs";
-import uuid from "uuid/v4";
+import { generateUserId } from "../util/generateUserId";
 
 /**
  * GET /login
@@ -68,60 +67,28 @@ export const postRegister = (req: Request, res: Response, next: NextFunction) =>
     });
   }
 
-  // validation passed, ready to store in database
-  else {
-    // find if referent code exists
-    User.findOne({ referent: referent })
-      .then(user => {
-        if (user) {
-          // find if email is already registered
-          User.findOne({ email: email })
-            .then(user => {
-              if (user) {
-                // if email already exists, render page with error message
-                errors.push("email is already registered");
-                res.render("account/register", {
-                  title: "register",
-                  errors,
-                  firstName,
-                  lastName,
-                  email,
-                  phone,
-                  address,
-                  referent
-                });
-              } else {
-                // create user
-                var newUser = new User({
-                  // TODO: check if id already exists in database
-                  id: uuid(),
-                  firstName,
-                  lastName,
-                  email,
-                  phone,
-                  address,
-                  password,
-                  referent: generateReferentCode(firstName, lastName)
-                });
+  // create new user, generate user id and referent code
+  const newUser = new User({
+    id: generateUserId(),
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phone: phone,
+    address: address,
+    password: password,
+    referent: generateReferentCode(firstName, lastName)
+  });
 
-                // generate salt and hash password
-                // TODO: move password encryption to models/user.ts (see microsoft template)
-                bcrypt.genSalt(10, (err, salt) =>
-                  bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    // store hashed password
-                    newUser.password = hash;
-                    // save new user and redirect to login page
-                    newUser.save()
-                      .then(user => { res.redirect('/auth/login') })
-                      .catch(err => console.error(err));
-                  }));
-              }
-            });
-        } else {
-          // referent code couldn't be found, render page with error message
-          errors.push("referent code does not exist");
-          res.render("account/register", {
+  // find referent code in database
+  User.findOne({ referent: referent }, (err, existingReferent) => {
+    if (err) { return next(err); }
+    if (existingReferent) {
+      // find if email is already registered
+      User.findOne({ email: email }, (err, existingUser) => {
+        if (err) { return next(err); }
+        if (existingUser) {
+          errors.push("email is already registered");
+          return res.render("account/register", {
             title: "register",
             errors,
             firstName,
@@ -132,6 +99,25 @@ export const postRegister = (req: Request, res: Response, next: NextFunction) =>
             referent
           });
         }
+
+        // save user in database
+        newUser.save((err) => {
+          if (err) { return next(err); }
+          return res.redirect("/");
+        });
       });
-  }
+    } else {
+      errors.push("referent code does not exist");
+      return res.render("account/register", {
+        title: "register",
+        errors,
+        firstName,
+        lastName,
+        email,
+        phone,
+        address,
+        referent
+      });
+    }
+  });
 };
---
+++ src/util/generateUserId.ts
@@ -0,0 +1,15 @@
+import { User } from "../models/user";
+import uuid from "uuid/v4";
+
+export function generateUserId(): string {
+    // generate user id
+    var userId: string = uuid();
+
+    // check in database if already exists
+    User.findOne({ id: userId }, (err, existingId) => {
+        if (err) { return false; }
+        // call function again to generate a new id and check again
+        if (existingId) { generateUserId(); }
+    });
+    return userId;
+}
