a479f2f6f76977de36b9225d6b4973ae940204e7
Philippe Loctaux <p@philippeloctaux.com>
2019-07-08 15:00:35 +0200

    added automatic timestamps + lowercase for email

--- src/models/user.ts
+++ src/models/user.ts
@@ -10,21 +10,18 @@ export type UserDocument = mongoose.Document & {
   address: string;
   password: string;
   referent: string;
-  creationDate: Date;
 };
 
 const UserSchema = new mongoose.Schema({
-  // TODO: see if 'required' is really useful, see best practices for mongodb schema
   id: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, lowercase: true, required: [ true, "can't be blank" ], unique: true, index: true },
   phone: { type: String, required: true },
   address: { type: String, required: true },
   password: { type: String, required: true },
   referent: { type: String, required: true },
-  creationDate: { type: Date, default: Date.now }
-});
+}, { timestamps: true });
 
 /**
  * Password hashing
