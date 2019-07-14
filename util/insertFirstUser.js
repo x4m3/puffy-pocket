0501671bb5fadb961aab46cb01a8a15c894386c3
Philippe Loctaux <p@philippeloctaux.com>
2019-07-14 22:08:50 +0200

    insertFirstUser.js: cleaned up code

--- util/insertFirstUser.js
+++ util/insertFirstUser.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var database = "puffy-pocket";
+var collection = "users";
+
 var newUser = {
   userId: "00000000-0000-0000-0000-000000000000",
   email: "",
@@ -18,14 +23,14 @@ var newUser = {
   }
 };
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("puffy-pocket");
-  dbo.collection("users").insertOne(newUser, function(err, res) {
-    if (err) throw err;
+  var dbo = db.db(database);
+  dbo.collection(collection).insertOne(newUser, function(err, res) {
+    if (err) {
+      console.error(err.errmsg);
+      process.exit(1);
+    }
     console.log(newUser);
     console.log("1 document inserted");
     db.close();
