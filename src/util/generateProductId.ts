22fc0b1f0cb44d6d9b35ad59034a431c517dd251
Philippe Loctaux <p@philippeloctaux.com>
2019-07-19 12:07:27 +0200

    fixed indentation, added generateProductId

---
+++ src/util/generateProductId.ts
@@ -0,0 +1,15 @@
+import { Product } from "../models/product";
+import uuid from "uuid/v4";
+
+export function generateProductId(): string | boolean {
+  // generate product id
+  var productId: string = uuid();
+
+  // check in database if already exists
+  Product.findOne({ productId: productId }, (err, existingId) => {
+    if (err) { return false; }
+    // call function again to generate a new id and check again
+    if (existingId) { generateProductId(); }
+  });
+  return productId;
+};
--- src/util/generateUserId.ts
+++ src/util/generateUserId.ts
@@ -2,14 +2,14 @@ import { User } from "../models/user";
 import uuid from "uuid/v4";
 
 export function generateUserId(): string | boolean {
-    // generate user id
-    var userId: string = uuid();
+  // generate user id
+  var userId: string = uuid();
 
-    // check in database if already exists
-    User.findOne({ userId: userId }, (err, existingId) => {
-        if (err) { return false; }
-        // call function again to generate a new id and check again
-        if (existingId) { generateUserId(); }
-    });
-    return userId;
+  // check in database if already exists
+  User.findOne({ userId: userId }, (err, existingId) => {
+    if (err) { return false; }
+    // call function again to generate a new id and check again
+    if (existingId) { generateUserId(); }
+  });
+  return userId;
 }
