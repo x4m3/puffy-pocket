955a3bf727bacb13bb0113bf0f51a297c9af4c03
Philippe Loctaux <p@philippeloctaux.com>
2019-07-22 16:03:49 +0200

    checking mime type before saving in database

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import { Product, ProductDocument } from "../models/product";
 import { User } from "../models/user";
 import { generateProductId } from "../util/generateProductId";
+import { checkFileImage } from "../util/checkFileImage";
 
 /**
  * GET /admin
@@ -63,7 +64,16 @@ export const getUsers = (req: Request, res: Response, next: NextFunction) => {
  * Add new product to database
  */
 export const postProductsAdd = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: check if uploaded file is an image
+  // if uploaded file is not an image
+  if (checkFileImage(req.files.image.name) == false) {
+    // delete uploaded file from filesystem
+    fs.unlink(req.files.image.path, (err) => {
+      if (err) throw err;
+    });
+
+    // redirect back to the products page
+    return res.redirect("/admin/products");
+  }
 
   const newProduct: ProductDocument = new Product({
     productId: generateProductId(),
@@ -73,13 +83,19 @@ export const postProductsAdd = (req: Request, res: Response, next: NextFunction)
     image: {
       data: fs.readFileSync(req.files.image.path),
       contentType: req.files.image.type
-    }
+    },
+    available: true
   });
 
   newProduct.save((err) => {
     if (err) { return next(err); }
-    console.log("new product saved");
-    res.contentType(newProduct.image.contentType);
-    res.send(newProduct.image.data);
+
+    // delete uploaded file from filesystem
+    fs.unlink(req.files.image.path, (err) => {
+      if (err) throw err;
+    });
+
+    // redirect back to the products page
+    return res.redirect("/admin/products");
   });
 };
---
+++ src/util/checkFileImage.ts
@@ -0,0 +1,18 @@
+import mimeTypes from "mime-types";
+
+/**
+ * Checks if file is an image or not
+ * @param   {string} fileName Name of the file to check
+ * @returns {boolean} True if file is an image, false if not
+ */
+export function checkFileImage(fileName: string) : boolean {
+  const mime = mimeTypes.lookup(fileName);
+
+  console.log(mime);
+  switch (mime) {
+    case "image/jpeg": return true; break;
+    case "image/png": return true; break;
+    case "image/webp": return true; break;
+  }
+  return false;
+};
