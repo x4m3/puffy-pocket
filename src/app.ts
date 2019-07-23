063f4bbab5b0e3de1ab57b5e96de88e356380acf
Philippe Loctaux <p@philippeloctaux.com>
2019-07-23 20:51:31 +0200

    delete user with GET instead of POST, can edit user (need to fix password)

--- src/app.ts
+++ src/app.ts
@@ -108,7 +108,9 @@ app.get("/admin", isAuthenticated, isAdmin, adminController.getIndex);
 app.get("/admin/products", isAuthenticated, isAdmin, adminController.getProducts);
 app.post("/admin/products/add", isAuthenticated, isAdmin, ExpressFormidable({ hash: "sha1" }), adminController.postProductsAdd);
 app.get("/admin/users", isAuthenticated, isAdmin, adminController.getUsers);
-app.post("/admin/users/delete", isAuthenticated, isAdmin, adminController.postUserDelete);
+app.get("/admin/users/delete/:userId", isAuthenticated, isAdmin, adminController.getUserDelete);
+app.get("/admin/users/edit/:userId", isAuthenticated, isAdmin, adminController.getUserEdit);
+app.post("/admin/users/edit/:userId", isAuthenticated, isAdmin, adminController.postUserEdit);
 app.get("/products/:productId/image", isAuthenticated, productController.getProductImage);
 app.get("/products/:productId/thumbnail", isAuthenticated, productController.getProductThumbnail);
 
--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -123,15 +123,105 @@ export const postProductsAdd = (req: Request, res: Response, next: NextFunction)
 };
 
 /**
- * POST /admin/users/delete
- * Delete user
+ * GET /admin/users/delete/:userId
+ * Delete user from "userId"
  */
-export const postUserDelete = (req: Request, res: Response, next: NextFunction) => {
-  // find user via userId in database and remove it
-  User.findOneAndDelete({ userId: req.body.userId }, (err) => {
+export const getUserDelete = (req: Request, res: Response, next: NextFunction) => {
+  // find user via userId in database
+  User.findOne({ userId: req.params.userId }, (err, userToDelete) => {
     if (err) { return next(err); }
+    if (userToDelete) {
+      // delete user
+      userToDelete.remove((err, removedUser) => {
+        if (err) { return next(err); }
+        console.log("deleted");
+        // redirect back to the users page
+        return res.redirect("/admin/users");
+      });
+    } else {
+      // if userId is invalid, return 404
+      return next(err);
+    }
   });
+};
 
-  // redirect back to the users page
-  return res.redirect("/admin/users");
+/**
+ * GET /admin/users/edit/:userId
+ * Display edit user information from "userId"
+ */
+export const getUserEdit = (req: Request, res: Response, next: NextFunction) => {
+  // try to find user in database
+  User.findOne({ userId: req.params.userId}, (err, user) => {
+    if (err) { return next(err); }
+    if (user) {
+      return res.render("admin/user-edit", {
+        title: "edit user - admin panel",
+        userId: user.userId,
+        firstName: user.info.name.first,
+        lastName: user.info.name.last,
+        email: user.email,
+        phone: user.info.phone,
+        address: user.info.address,
+        admin: user.admin
+      })
+    }
+    // if userId is invalid, return 404
+    return next(err);
+  });
+};
+
+/**
+ * POST /admin/users/edit/:userId
+ * Edit user information from "userId"
+ */
+export const postUserEdit = (req: Request, res: Response, next: NextFunction) => {
+  let firstName: string = req.body.firstName;
+  let lastName: string = req.body.lastName;
+  let email: string = req.body.email;
+  let phone: string = req.body.phone;
+  let address: string = req.body.address;
+  let admin: boolean = (req.body.admin) == "true" ? true : false;
+
+  let errors: Array<string> = [];
+
+  if (firstName.length != 0 && !firstName.match(/^[A-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/)) {
+    errors.push("only letters are allowed for name");
+  }
+  if (lastName.length != 0 && !lastName.match(/^[A-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/)) {
+    errors.push("only letters are allowed for name");
+  }
+
+  if (phone.length != 0 && !phone.match(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/)) {
+    errors.push("invalid phone number");
+  }
+
+  if (errors.length > 0) {
+    // render page again with user info and error messages
+    return res.redirect("/admin/users/edit/" + req.params.userId);
+  }
+
+  // find user in database
+  User.findOne({ userId: req.params.userId }, (err, user) => {
+    if (err) { return next(err); }
+    if (user) {
+      // update user info if they have changed
+      if (firstName.length != 0) { user.info.name.first = firstName; }
+      if (lastName.length != 0) { user.info.name.last = lastName; }
+      if (email.length != 0) { user.email = email; }
+      if (phone.length != 0) { user.info.phone = phone; }
+      if (address.length != 0) { user.info.address = address; }
+
+      // change admin status
+      if (admin == true && user.admin == false) { user.admin = true; }
+      if (admin == false && user.admin == true) { user.admin = false; }
+
+      // save changes in database
+      user.save((err) => {
+        if (err) { return next(err); }
+
+        // redirect back to the users page
+        return res.redirect("/admin/users");
+      })
+    }
+  });
 };
