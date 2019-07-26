44309121b512d7cf64a736cf92eea700cf7c37bb
Philippe Loctaux <p@philippeloctaux.com>
2019-07-26 15:59:29 +0200

    starting to display products on homepage

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,12 +1,41 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import { Product } from "../models/product";
 
 /**
  * GET /
  * home page
  */
-export const index = (req: Request, res: Response) => {
-  res.render("index", {
-    title: "homepage",
-    user: req.user
-  });
+export const index = (req: Request, res: Response, next: NextFunction) => {
+  // if user is not logged in, return public homepage
+  if (req.isUnauthenticated()) {
+    return res.render("index-public", {
+      title: "homepage"
+    });
+  }
+
+  type productData = {
+    name: string;
+    price: number;
+    points: number;
+    image: string;
+    thumbnail: string;
+  };
+  let productList: Array<productData> = [];
+
+  Product.find({ available: true }, (err, products) => {
+    if (err) { return next(err); }
+    products.forEach((product) => {
+      productList.push({
+        name: product.name,
+        price: product.price,
+        points: product.points,
+        image: "/products/" + product.productId + "/image",
+        thumbnail: "/products/" + product.productId + "/image?width=250"
+      });
+    });
+    res.render("index", {
+      title: "homepage",
+      products: productList
+    });
+  }).limit(3);
 };
